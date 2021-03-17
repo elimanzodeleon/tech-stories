9622766654612c787d19704f7023760bd2cb90a9
elimanzodeleon <elimanzodeleon@gmail.com>
2021-03-17 15:52:17 -0700

    changed to https for api requests

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 // const main api endpoint used for fetching stories
 export const API_HOME_ENDPOINT =
-  'http://hn.algolia.com/api/v1/search?tags=front_page';
+  'https://hn.algolia.com/api/v1/search?tags=front_page';
 export const API_SEARCH_ENDPOINT =
   'https://hn.algolia.com/api/v1/search?query=';
 export const API_PAGE_NUM = '&page=';
